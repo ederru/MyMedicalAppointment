d8a69b515fb0692d1ac8efa2addd88323c46ea1b
Eder Ruiz <0805ederruiz@gmail.com>
2021-09-07 15:29:55 -0500

    Initial commit

---
+++ src/Doctor.java
@@ -0,0 +1,42 @@
+public class Doctor {
+    //Atributos
+    static int id = 0;//AutoIncrement
+    String name;
+    String speciality;
+
+    public Doctor(String name, String speciality) {
+        this.name = name;
+        this.speciality = speciality;
+        id++;
+    }
+
+    //Comportamientos
+
+    public void valores() {
+        System.out.println("ID Doctor: " + getId() + " Doctor Name: " + getName() + " Doctor Especiality: " + getSpeciality());
+    }
+
+    public void showID() {
+        System.out.println("ID Doctor is: " + getId());
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor("Eder Ruíz", "Cardiólogo");
+        myDoctor.valores();
+        Doctor myDoctor2 = new Doctor("Paola Slagado", "Neuro");
+        myDoctor2.valores();
+    }
+}
